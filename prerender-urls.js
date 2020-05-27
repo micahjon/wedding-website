1ccbc9a2437a8ca8ea58b85ed7e4404175914eb8
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2020-05-26 22:44:05 -0700

    Pre-render all pages

---
+++ prerender-urls.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+  return [
+    {
+      url: '/',
+      title: "Audrey & Micah's Wedding",
+    },
+    {
+      url: '/rsvp',
+      title: 'RSVP',
+    },
+    {
+      url: '/registry',
+      title: 'Registry',
+    },
+  ];
+};
